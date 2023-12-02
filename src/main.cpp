a77ed72498df6daeae5b25117e0180dbe6bc3d10
Noam Zeise <noam4134@gmail.com>
2023-12-02 00:30:08 +0000

    camera testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,54 @@
 
 int main() {
     ManagerState state;
-    state.cursor = cursorState::disabled;
+    state.cursor = cursorState::normal;
     state.windowTitle = "bmjam";
     state.windowWidth = 800;
     state.windowHeight = 600;
+    state.conf.multisampling = true;
+    state.conf.sample_shading = true;
 
     Manager manager(state);
     ResourcePool* pool = manager.render->pool();
-    Resource::Model monkey = pool->model()->load("models/monkey.obj");
-    glm::mat4 monkeyMat = glm::translate(glm::rotate(glm::mat4(1.0f), glm::radians(270.0f),
-						     glm::vec3(-1.0f, 0.0f, 0.0f)),
-					 glm::vec3(0.0f, -8.0f, -10.0f));    
+    Resource::Model sphere = pool->model()->load("models/sphere.obj");
+    glm::mat4 sphereMat = glm::mat4(1.0f);
     Resource::Texture testImage = pool->tex()->load("textures/test.png");
+    Resource::Model monkey = pool->model()->load("models/monkey.obj");
+    glm::mat4 monkeyMat = glm::translate(glm::mat4(1.0f), glm::vec3(2, 0, 0));
+    
     manager.render->LoadResourcesToGPU(pool);
     manager.render->UseLoadedResources();
-    camera::FirstPerson cam;
+
+    glm::vec3 camPos(5, 0, 0);
+    glm::vec3 target(0);
+    glm::vec3 up(0, 0, 1);
+    
     while(!glfwWindowShouldClose(manager.window)) {
+	// Update
 	manager.update();
 	if(manager.input.kb.press(GLFW_KEY_ESCAPE))
 	    glfwSetWindowShouldClose(manager.window, GLFW_TRUE);
-	cam.update(manager.input, manager.timer);
-	manager.fov = cam.getZoom();
-	manager.render->set3DViewMat(cam.getViewMatrix(), cam.getPos());
 
+	if(manager.input.kb.hold(GLFW_KEY_UP)) {
+	    auto r = glm::quat(glm::vec3(0, 0.001 * manager.timer.dt(), 0));
+	    auto c = glm::conjugate(r);
+	    camPos = r * camPos * c;
+	}
+	if(manager.input.kb.hold(GLFW_KEY_DOWN)) {
+	    auto r = glm::quat(glm::vec3(0, -0.001 * manager.timer.dt(), 0));
+	    auto c = glm::conjugate(r);
+	    camPos = r * camPos * c;
+	}
+	
+	manager.render->set3DViewMat(glm::lookAt(camPos, target, up),
+				     camPos);
+	// Draw
 	if(manager.winActive()) {
-	    manager.render->DrawQuad(testImage,
+	    /*manager.render->DrawQuad(testImage,
 				     glmhelper::calcMatFromRect(
-					     glm::vec4(0, 0, 400, 600),
-					     0.0f, 0.001f));
-	    manager.render->DrawQuad(testImage,
-				     glmhelper::calcMatFromRect(
-					     glm::vec4(400, 0, 400, 600),
-					     0.0f, 4.9f));
+					     glm::vec4(0, 0, 800, 600),
+					     0.0f, 0.001f));*/
+	    manager.render->DrawModel(sphere, sphereMat, glm::inverseTranspose(sphereMat));
 	    manager.render->DrawModel(monkey, monkeyMat, glm::inverseTranspose(monkeyMat));
 	    std::atomic<bool> drawSubmitted;
 	    manager.render->EndDraw(drawSubmitted);
