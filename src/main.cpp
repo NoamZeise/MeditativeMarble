c765b7a0a70be59e6a633dfb2ec345f85a563cad
Noam Zeise <noam4134@gmail.com>
2023-12-05 01:35:21 +0000

    3rd person cam working without target movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <manager.h>
 #include <GameHelper/camera.h>
 #include <graphics/glm_helper.h>
-#include <glm/gtc/matrix_inverse.hpp> //for inverseTranspose
+#include <graphics/logger.h>
+#include <glm/gtc/matrix_inverse.hpp>
+
+void draw_vect(Render* render,
+	       Resource::Font font,
+	       std::string vname,
+	       glm::vec3 v,
+	       float y);
 
 int main() {
     ManagerState state;
-    state.cursor = cursorState::normal;
+    state.cursor = cursorState::disabled;
     state.windowTitle = "bmjam";
     state.windowWidth = 800;
     state.windowHeight = 600;
@@ -19,6 +26,7 @@ int main() {
     Resource::Texture testImage = pool->tex()->load("textures/test.png");
     Resource::Model monkey = pool->model()->load("models/monkey.obj");
     glm::mat4 monkeyMat = glm::translate(glm::mat4(1.0f), glm::vec3(2, 0, 0));
+    Resource::Font font = pool->font()->load("textures/Roboto-Black.ttf");
     
     manager.render->LoadResourcesToGPU(pool);
     manager.render->UseLoadedResources();
@@ -26,6 +34,10 @@ int main() {
     glm::vec3 camPos(5, 5, 0);
     glm::vec3 target(0);
     glm::vec3 up(0, 0, 1);
+
+    glm::vec3 camForward = glm::normalize(camPos - target);
+    glm::vec3 camLeft = glm::normalize(glm::cross(up, camForward));
+    glm::vec3 camUp = glm::normalize(glm::cross(camForward, camLeft));
     
     while(!glfwWindowShouldClose(manager.window)) {
 	// Update
@@ -33,9 +45,9 @@ int main() {
 	if(manager.input.kb.press(GLFW_KEY_ESCAPE))
 	    glfwSetWindowShouldClose(manager.window, GLFW_TRUE);
 
-	glm::vec3 camForward = glm::normalize(camPos - target);
-	glm::vec3 camLeft = glm::normalize(glm::cross(up, camForward));
-	glm::vec3 camUp = glm::normalize(glm::cross(camForward, camLeft));
+	camForward = glm::normalize(camPos - target);
+	camLeft = glm::normalize(glm::cross(up, camForward));
+	camUp = glm::normalize(glm::cross(camForward, camLeft));
 	glm::mat4 camView(1.0f);
 	camView[0][0] = camLeft.x;
 	camView[1][0] = camLeft.y;
@@ -50,47 +62,54 @@ int main() {
 	camView[2][2] = camForward.z;
 	camView[3][2] = -glm::dot(camForward, camPos);
 
-	if(manager.input.kb.hold(GLFW_KEY_UP)) {
-	    float size = -0.001 * manager.timer.dt();
-	    auto r = glm::quat(cos(size/2), (float)sin(size/2)*camLeft);
-	    r = glm::normalize(r);
-	    auto c = glm::conjugate(r);
-	    camPos = r * camPos * c;
-	}
-	if(manager.input.kb.hold(GLFW_KEY_DOWN)) {
+	float speed = 0.01 * manager.timer.dt();
+	if(manager.input.kb.hold(GLFW_KEY_UP))
+	    target.y -= speed;
+	if(manager.input.kb.hold(GLFW_KEY_DOWN))
+	    target.y += speed;
+	if(manager.input.kb.hold(GLFW_KEY_LEFT))
+	    target.x -= speed;
+	if(manager.input.kb.hold(GLFW_KEY_RIGHT))
+	    target.x += speed;
+	sphereMat = glm::translate(glm::mat4(1.0f), target);
+
+	float mouseSensitivity = 0.1f;
+	glm::vec2 ctrlDir(-manager.input.m.dx(), -manager.input.m.dy());
+	ctrlDir *= mouseSensitivity;
+	if(ctrlDir.x != 0 || ctrlDir.y != 0) {
+	    float updot = glm::dot(camForward, up);
+	    float lim = 0.9f;
+	    if(updot > lim && ctrlDir.y < 0 || -updot > lim && ctrlDir.y > 0)
+		ctrlDir.y = 0;
 	    float size = 0.001 * manager.timer.dt();
-	    auto r = glm::quat(cos(size/2), (float)sin(size/2)*camLeft);
-	    r = glm::normalize(r);
-	    auto c = glm::conjugate(r);
-	    camPos = r * camPos * c;
-	}
-	if(manager.input.kb.hold(GLFW_KEY_LEFT)) {
-	    float size = -0.001 * manager.timer.dt();
-	    auto r = glm::quat(cos(size/2), (float)sin(size/2)*camUp);
-	    r = glm::normalize(r);
-	    auto c = glm::conjugate(r);
-	    camPos = r * camPos * c;
-	}
-	if(manager.input.kb.hold(GLFW_KEY_RIGHT)) {
-	    float size = 0.001 * manager.timer.dt();
-	    auto r = glm::quat(cos(size/2), (float)sin(size/2)*camUp);
-	    r = glm::normalize(r);
-	    auto c = glm::conjugate(r);
-	    camPos = r * camPos * c;
+	    ctrlDir *= size/2;
+	    auto qx = glm::quat(cos(ctrlDir.x), (float)sin(ctrlDir.x)*up);
+	    auto qy = glm::quat(cos(ctrlDir.y), (float)sin(ctrlDir.y)*camLeft);
+	    auto q = qx * qy;
+	    auto c = glm::conjugate(q);
+	    camPos = q * camPos * c;
 	}
 	
 	manager.render->set3DViewMat(camView, camPos);
 	
 	// Draw
 	if(manager.winActive()) {
-	    /*manager.render->DrawQuad(testImage,
-				     glmhelper::calcMatFromRect(
-					     glm::vec4(0, 0, 800, 600),
-					     0.0f, 0.001f));*/
 	    manager.render->DrawModel(sphere, sphereMat, glm::inverseTranspose(sphereMat));
 	    manager.render->DrawModel(monkey, monkeyMat, glm::inverseTranspose(monkeyMat));
+	    draw_vect(manager.render, font, "cam pos", camPos, 50.0);
 	    std::atomic<bool> drawSubmitted;
 	    manager.render->EndDraw(drawSubmitted);
 	}
     }
+}
+
+void draw_vect(Render* render,
+	       Resource::Font font,
+	       std::string vname,
+	       glm::vec3 v, float y) {
+    render->DrawString(font, vname + ": ("
+		       + std::to_string(v.x) + ", "
+		       + std::to_string(v.y) + ", "
+		       + std::to_string(v.z) + ")",
+		       glm::vec2(10, y), 20, 0.9f, glm::vec4(1.0f)); 
 }
